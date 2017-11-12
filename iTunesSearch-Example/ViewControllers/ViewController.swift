66ae1ef7cc868444c642a35dba5e9e6746b40688
William Boles <william@memrise.com>
2017-11-12 11:56:45 +0000

    Initial xcode project

---
+++ iTunesSearch-Example/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesSearch-Example
+//
+//  Created by William Boles on 12/11/2017.
+//  Copyright © 2017 William Boles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
